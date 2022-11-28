7a3b55e90c50533e73f4c6260db32ed9ceb29ed4
andylamax <andylamax@programmer.net>
2022-11-29 01:48:44 +0300

    Added GoogleApiLocation Support

---
+++ geo/src/commonMain/kotlin/presenters/parsers/GooglePlacesApiParser.kt
@@ -0,0 +1,36 @@
+package presenters.parsers
+
+import geo.Country
+import geo.GeoLocation
+import kollections.toIList
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+
+internal class GooglePlacesApiParser {
+    private val codex = Json {
+        ignoreUnknownKeys = true
+        isLenient = true
+    }
+
+    fun parse(json: String): GeoLocation {
+        val resp = codex.decodeFromString<GooglePlaceApiResponse>(json)
+
+        val postalCode = resp.address_components.firstOrNull {
+            it.types.contains("postal_code")
+        }
+        val countryKey = resp.address_components.firstOrNull {
+            it.types.contains("country")
+        } ?: throw IllegalArgumentException("Failed to match a country type in address_component")
+
+        val country = Country.values().firstOrNull {
+            it.label.contains(countryKey.long_name, ignoreCase = true) || countryKey.short_name == it.name
+        } ?: throw IllegalArgumentException("Failed to parse country ${countryKey.long_name}(${countryKey.short_name})")
+
+        return GeoLocation(
+            lines = resp.description.split(", ").toIList(),
+            country = country,
+            cords = resp.geometry.location,
+            code = postalCode?.long_name
+        )
+    }
+}
