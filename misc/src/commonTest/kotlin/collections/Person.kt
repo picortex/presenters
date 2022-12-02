d4500fab2110fe6509617a1334e27c0c41b481dd
andylamax <andylamax@programmer.net>
2022-12-02 15:26:36 +0300

    Purged unwanted code in unwated placed

--- misc/src/commonTest/kotlin/collections/Person.kt
+++
@@ -1,7 +0,0 @@
-package collections
-
-data class Person(val name: String = "Andy", val age: Int = 12) {
-    companion object {
-        val List = List(25) { Person("Anderson $it", age = 15 + it) }
-    }
-}
