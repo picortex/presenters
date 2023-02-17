a6710f591e0db2f6156613689b169a8a432aa7db
andylamax <andylamax@programmer.net>
2023-02-17 15:11:36 +0300

    Releasing

--- inputs/core/src/commonMain/kotlin/presenters/Form.kt
+++ inputs/core/src/commonMain/kotlin/presenters/Form.kt
@@ -28,11 +28,11 @@ import presenters.validation.throwIfInvalid
 import viewmodel.ViewModel
 import kotlin.js.JsExport
 
-open class Form<out F : Fields, P, out R>(
+open class Form<out F : Fields, out P, out R>(
     open val heading: String,
     open val details: String,
     open val fields: F,
-    open val config: FormConfig<P>,
+    open val config: FormConfig<@UnsafeVariance P>,
     initializer: FormActionsBuildingBlock<P, R>,
 ) : ViewModel<FormState<R>>(config.of(Pending)) {
 
